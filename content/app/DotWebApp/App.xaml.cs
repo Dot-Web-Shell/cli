5277c20bb8f12f9f8c4f2892b8ab07e981e2d391
Akash Kava <39438041+ackava@users.noreply.github.com>
2024-08-09 13:03:52 +0530

    Added user agent

--- content/app/DotWebApp/App.xaml.cs
+++ content/app/DotWebApp/App.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using NativeShell.Pages;
 using Microsoft.Extensions.Configuration;
+using Maui
 
 namespace DotWebApp;
 
@@ -11,8 +12,32 @@ public partial class App : Application
 
 		var url = config.GetSection("App")?.GetValue<string>("Url");
 
-		MainPage = new NativeShellMainPage() {
+		var deviceType = "Desktop";
+		if (DeviceInfo.Idiom == DeviceIdiom.Phone)
+		{
+			deviceType = "Mobile";
+		}
+		if (DeviceInfo.Idiom == DeviceIdiom.Tablet)
+		{
+			deviceType = "Tablet";
+		}
+
+		var mainPage = new NativeShellMainPage()
+		{
 			Url = url
 		};
-	}
+
+		var webView = mainPage.WebView;
+
+		if (DeviceInfo.Platform == DevicePlatform.iOS)
+		{
+			webView.UserAgent = $"Mobile-App/1.0 (iPhone; iOS {DeviceInfo.VersionString}; {deviceType}) XamarinApp/{AppInfo.VersionString} MauiApp/{AppInfo.VersionString}";
+		}
+		if (DeviceInfo.Platform == DevicePlatform.Android)
+		{
+			webView.UserAgent = $"Mobile-App/1.0 (Android; {deviceType}) XamarinApp/{AppInfo.VersionString} MauiApp/{AppInfo.VersionString}";
+		}
+
+		MainPage = mainPage;
+ 	}
 }
